2b3eb728891b1520b21b3b9b33cfdafd79c6777b
Gabe <Carrollgbusiness@gmail.com>
2026-04-10 16:18:18 -0500

    Size of sandwich and price

--- src/main/java/com/pluralsight/PontOfSales.java
+++ src/main/java/com/pluralsight/PontOfSales.java
@@ -1,10 +1,23 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PontOfSales {
     public static void main(String[] args) {
-       String size;
-       double price;
-       int age;
+        //allows user inputs
+        Scanner input = new Scanner(System.in);
+        System.out.print("What size sandwich do you want Regular(1) or Large(2): ");
+        int sandwichSize = input.nextInt();
+        //Var
+        double price = 0;
+
+        if (sandwichSize == 1) {
+            price = 5.45;
+        } else if (sandwichSize == 2) {
+            price = 8.95;
+        }
+        System.out.print("your price is "  + price);
+
 
     }
 }
