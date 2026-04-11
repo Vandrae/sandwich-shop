3f93d7beac72c517148a621b7bcf9028a86fddd9
Gabe <Carrollgbusiness@gmail.com>
2026-04-11 11:38:11 -0500

    Added Discount conditional for age brackets, Formatted output to 0.00

--- src/main/java/com/pluralsight/PontOfSales.java
+++ src/main/java/com/pluralsight/PontOfSales.java
@@ -4,20 +4,30 @@ import java.util.Scanner;
 
 public class PontOfSales {
     public static void main(String[] args) {
-        //allows user inputs
         Scanner input = new Scanner(System.in);
+        //------------------User Interactions------------------
         System.out.print("What size sandwich do you want Regular(1) or Large(2): ");
         int sandwichSize = input.nextInt();
-        //Var
+        System.out.print("How old are you? ");
+        int age = input.nextInt();
         double price = 0;
 
+        //determines price based on sandwich size
         if (sandwichSize == 1) {
             price = 5.45;
         } else if (sandwichSize == 2) {
             price = 8.95;
         }
-        System.out.print("your price is "  + price);
 
+        // provides discounts based on age
+        if (age <= 17){
+            price = price * .9;
+        } else if (age >= 65 ) {
+            price = price * .8;
+        }
+
+        //Prints price using above if statement changes
+        System.out.printf("Your total is $"  + "%.2f", price);
 
     }
 }
